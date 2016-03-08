85310564d637ad55bdc60821c7111a5f7000f37d
Rolando Cosme <rolandocosme@Rolandos-MacBook-Pro.local>
2016-03-08 15:54:40 -0500

    changed site from reddit to slickdeals.net and pulled titles and links info

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
-
-request('https://www.reddit.com/', function (error, response, html) {
+request('http://slickdeals.net/', function (error, response, html) {
   var $ = cheerio.load(html);
   var result = [];
-  $('p.title').each(function(i, element){
-    var a = $(this);
-    console.log(a.text());
+  $('.itemImageLink').each(function(i, element){
+    var a = $(this).text();
+    var link = $(this).attr('href');
+    var title = $(this).attr('title');
     //scrape some stuff, put it in an object and add it to the result array
-
+    result.push({
+      title: title,
+      link: link
+    })
     });
   console.log(result);
 });
+
