a74aa072b3bc47eed4f3965d188474c326919800
Rolando Cosme <rolandocosme@nbp-133-81.nbp.ruw.rutgers.edu>
2016-03-07 21:26:44 -0500

    scraping reddit works now

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ var request = require('request');
 var cheerio = require('cheerio');
 
 
-request('[http://www.cnet.com/]', function (error, response, html) {
+request('https://www.reddit.com/', function (error, response, html) {
   var $ = cheerio.load(html);
   var result = [];
-  $(['h3.title']).each(function(i, element){
-
+  $('p.title').each(function(i, element){
+    var a = $(this);
+    console.log(a.text());
     //scrape some stuff, put it in an object and add it to the result array
 
     });
