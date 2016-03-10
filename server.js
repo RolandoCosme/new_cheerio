0abee20264e554046053f94d396455833b1f75c5
Rolando Cosme <rolandocosme@nbp-130-87.nbp.ruw.rutgers.edu>
2016-03-09 20:46:32 -0500

    added routes and port

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 var request = require('request');
 var cheerio = require('cheerio');
+var express = require('express');
+var mongojs = require('mongojs');
+var app = express();
+
+var PORT = process.env.PORT || 4000;
+var db = mongojs ('slickdeals', ['deals']);
+
+db.on('error', function (err){
+  console.error('Database error, err');
+});
+
+app.get('/', function (req , res){
+  res.send('You got deals!');
+});
 
 request('http://slickdeals.net/', function (error, response, html) {
   var $ = cheerio.load(html);
@@ -16,3 +30,6 @@ request('http://slickdeals.net/', function (error, response, html) {
   console.log(result);
 });
 
+app.listen(PORT, function (){
+  console.log('up and running on port %s', PORT);
+});
